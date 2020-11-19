fcb33aa900d716787cb8eaf6a296f9e275f0d2a0
marieXco <marie.collin98@gmail.com>
2020-11-19 09:41:39 +0100

    commit docker-compose V1

--- episen-sca-mco-back/src/main/java/com/example/episenscamcoback/endpoints/MonController.java
+++ episen-sca-mco-back/src/main/java/com/example/episenscamcoback/endpoints/MonController.java
@@ -3,7 +3,11 @@ package com.example.episenscamcoback.endpoints;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @RestController
 public class MonController {
@@ -16,6 +20,23 @@ public class MonController {
         PrintWriter writer = new PrintWriter("./orders.txt");
         writer.println(order);
         writer.close();
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("./logs.txt");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            System.out.println("dans le try du log");
+            logger.info(order);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
