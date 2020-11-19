4a727dc90f03bc66a9b145b491b957f5780cefb1
marieXco <marie.collin98@gmail.com>
2020-11-19 11:41:27 +0100

    commit docker-compose V2
    correction volumes

--- episen-sca-mco-back/src/main/java/com/example/episenscamcoback/endpoints/MonController.java
+++ episen-sca-mco-back/src/main/java/com/example/episenscamcoback/endpoints/MonController.java
@@ -3,11 +3,8 @@ package com.example.episenscamcoback.endpoints;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+
 
 @RestController
 public class MonController {
@@ -17,26 +14,9 @@ public class MonController {
     @CrossOrigin("*")
     public void add_order(@RequestBody String order) throws FileNotFoundException {
         System.out.println(order);
-        PrintWriter writer = new PrintWriter("./orders.txt");
+        PrintWriter writer = new PrintWriter("/orders/orders.txt");
         writer.println(order);
         writer.close();
-        Logger logger = Logger.getLogger("MyLog");
-        FileHandler fh;
-
-        try {
-            fh = new FileHandler("./logs.txt");
-            logger.addHandler(fh);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-            System.out.println("dans le try du log");
-            logger.info(order);
-
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
 }
